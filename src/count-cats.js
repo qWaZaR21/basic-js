2f06d7c1bdc168b5aeafab9210f1f7b10aae866b
Pavel Kulesh <kulesh.p.v.b.2.m@gmail.com>
2020-02-19 22:13:02 +0300

    feat: count-cat

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,11 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
+module.exports = function countCats(array) {
+  // throw 'Not implemented';
   // remove line with error and write your code here
+  let count = 0;
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array[i].length; j++) {
+      if (array[i][j] === "^^") count++;
+    }
+  }
+  return count;
 };

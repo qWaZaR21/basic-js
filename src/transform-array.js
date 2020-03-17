613e53bdb8c14742c3d85bae4e5fb929d96da467
Pavel Kulesh <kulesh.p.v.b.2.m@gmail.com>
2020-03-17 21:38:01 +0300

    add transform-array solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,25 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if (!(arr instanceof Array)) throw 'Bad params';
+    const resultArray = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        switch(arr[i]) {
+            case '--discard-next':
+                i++;
+                break;
+            case '--discard-prev':
+                resultArray.pop();
+                break;
+            case '--double-next':
+                if (i !== arr.length - 1) resultArray.push(arr[i + 1]);
+                break;
+            case '--double-prev':
+                if (i !== 0) resultArray.push(arr[i - 1]);
+                break;
+            default:
+                resultArray.push(arr[i]);
+                break;
+        }
+    }
+    return resultArray;
 };

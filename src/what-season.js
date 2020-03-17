64093c654ca2f5ee05d3151009bf4cece538666a
Pavel Kulesh <kulesh.p.v.b.2.m@gmail.com>
2020-03-17 21:10:16 +0300

    add solution

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,7 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (!date || date instanceof Date) throw 'Not implemented';
+  if (date.getMonth() === 0 || date.getMonth() === 1 || date.getMonth() === 11) return 'winter';
+  if (date.getMonth() === 2 || date.getMonth() === 3 || date.getMonth() === 4) return 'spring';
+  if (date.getMonth() === 5 || date.getMonth() === 6 || date.getMonth() === 7) return 'summer';
+  if (date.getMonth() === 8 || date.getMonth() === 9 || date.getMonth() === 10) return 'autumn';
 };

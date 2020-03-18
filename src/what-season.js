3593e75ba43e2ca3811a94856a60f7c0d7d57791
Pavel Kulesh <kulesh.p.v.b.2.m@gmail.com>
2020-03-18 23:44:14 +0300

    fix solution

--- src/what-season.js
+++ src/what-season.js
@@ -1,7 +1,8 @@
 module.exports = function getSeason(date) {
-  if (!date || date instanceof Date) throw 'Not implemented';
+  if (!date) return 'Unable to determine the time of year!';
+  if (!(Object.prototype.toString.call(date) === '[object Date]')) throw new Error('Bad params');
   if (date.getMonth() === 0 || date.getMonth() === 1 || date.getMonth() === 11) return 'winter';
   if (date.getMonth() === 2 || date.getMonth() === 3 || date.getMonth() === 4) return 'spring';
   if (date.getMonth() === 5 || date.getMonth() === 6 || date.getMonth() === 7) return 'summer';
   if (date.getMonth() === 8 || date.getMonth() === 9 || date.getMonth() === 10) return 'autumn';
-};
+};  
